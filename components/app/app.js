ca80e462d4c03484be3ba53897fe97cae5c42395
Grigory Semakin <grinsdot1@gmail.com>
2019-10-20 17:16:08 +0300

    form needed

---
+++ components/app/app.js
@@ -0,0 +1,51 @@
+(function() {
+    'use strict';
+
+    const Menu = window.Menu;
+
+    class App { 
+        constructor({el}) {
+
+            this.menu = new Menu({
+                el: el.querySelector('.js-menu'),
+                data: {
+                    title: 'Сайты',
+                    items: []
+                }
+
+            });
+
+            this.menu.setData({
+                title: 'Сайты2',
+                items: [
+                    {title: 'Первый'},
+                    {
+                      title: 'Второй',
+                      items: [
+                        {title: 'Второй-первый'},  
+                        {
+                          title: 'Второй-второй',
+                          items: [
+                            {title: 'Второй-второй-1'},
+                            {title: 'Второй-второй-2'},
+                            {title: 'Второй-второй-3'}
+                          ]
+                        },  
+                      ]
+                    },
+                    {title: 'Третий'},
+                    {title: 'Четвертый'}
+                  ]
+            });
+
+            
+
+        }
+    }
+
+    // export
+    window.App = App;
+    
+
+
+})();
